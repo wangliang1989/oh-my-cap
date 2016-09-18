48658abb3efb5cde3611e7dad16db242c5a97557
王亮 <wangliang0222@gmail.com>
2016-09-18 21:22:05 +0800

    example v1

---
+++ example/eventinfo.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use POSIX qw(strftime);
+$ENV{SAC_DISPLAY_COPYRIGHT} = 0;
+
+@ARGV == 1 or die "Usage: perl $0 dir\n";
+my ($dir) = @ARGV;
+
+chdir $dir;
+
+open(INFO, "< info") or die "Cannot find event info.";
+my $eventinfo = <INFO>;
+my ($origin, $evla, $evlo, $evdp, $mag) = split " ", $eventinfo;
+close(INFO);
+
+# 对发震时刻做解析
+my ($date, $time) = split "T", $origin;
+my ($year, $month, $day) = split "-", $date;
+my ($hour, $minute, $second) = split ":", $time;
+# 秒和毫秒均为整数
+my ($sec, $msec) = split /\./, $second;
+$msec = int(($second - $sec) * 1000 + 0.5);
+
+# 计算发震日期是一年中的第几天
+my $jday = strftime("%j", $second, $minute, $hour, $day, $month-1, $year-1900);
+
+open(SAC, "| sac") or die "Error in opening SAC\n";
+print SAC "wild echo off\n";
+print SAC "r *.SAC\n";
+print SAC "synchronize\n";  # 同步所有文件的参考时刻
+print SAC "ch o gmt $year $jday $hour $minute $sec $msec\n";
+print SAC "ch allt (0 - &1,o&) iztype IO\n";
+print SAC "ch evlo $evlo evla $evla evdp $evdp mag $mag\n";
+print SAC "wh\n";
+print SAC "q\n";
+close(SAC);
+
+chdir "..";
---
+++ example/process.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $usage = "
+本脚本的作用是完成某一个目录下的地震数据的预处理\n
+参数为一个或多个目录\n
+";
+
+@ARGV >= 1 or die "用法: $usage";
+
+foreach my $event (@ARGV) {
+    system "perl rdseed.pl $event";
+    system "perl merge.pl $event";
+    system "perl rename.pl $event";
+    system "perl eventinfo.pl $event";
+    system "perl marktime.pl $event";
+    system "perl transfer.pl $event";
+    system "perl rotate.pl $event";
+    system "perl resample.pl $event 0.2";
+}
