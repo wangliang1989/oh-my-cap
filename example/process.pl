bfa27292bdd227b5153de1d2ec6ff8d3f8db7ca0
Dongdong Tian <seisman.info@gmail.com>
2016-10-18 22:04:15 +0800

    doc update v3

--- example/process.pl
+++ example/process.pl
@@ -2,12 +2,7 @@
 use strict;
 use warnings;
 
-my $usage = "
-本脚本的作用是完成某一个目录下的地震数据的预处理\n
-参数为一个或多个目录\n
-";
-
-@ARGV >= 1 or die "用法: $usage";
+@ARGV >= 1 or die "Usage: perl $0 event1 event2 ... eventn\n";
 
 foreach my $event (@ARGV) {
     system "perl rdseed.pl $event";
--- example/weight.pl
+++ example/weight.pl
@@ -6,10 +6,11 @@ $ENV{SAC_DISPLAY_COPYRIGHT} = 0;
 @ARGV == 1 or die "Usage: perl $0 dir\n";
 my ($dir) = @ARGV;
 
-chdir $dir;
-
 my ($w1, $w2, $w3, $w4, $w5) = (1, 1, 1, 1, 1);
+my $ts = 0;
+my $ddist = 5;
 
+chdir $dir;
 open(IN, "saclst dist t0 t1 f *.z | sort -k2n |");
 my @lines = <IN>;
 close(IN);
@@ -18,19 +19,14 @@ open (OUT, '> ./weight.dat');
 foreach my $line (@lines) {
     my ($fname, $dist, $t0, $t1) = split /\s+/, $line;
     my ($net_sta) = split /\./, $fname;
-    my $tp;
-    my $ts = 0;
     # 震中距简化为5公里的倍数
-    $dist = (int(($dist + 2.5)/5))*5;
+    $dist = int($dist/$ddist + 0.5) * $ddist;
 
-    # 如果没有手动标定到时t1，就用理论到时t0代替,如果理论到时也没有则记为0
-    $tp = $t1;
-    if ($tp == -12345) {
-       $tp = $t0;
-    }
-    if ($tp == -12345) {
-        $tp = 0;
-    }
-    
+    # 如果没有手动标定到时t1，就用理论到时t0代替，如果理论到时也没有则记为0
+    my $tp = 0;
+    $tp = $t0 if $t0 != -12345;
+    $tp = $t1 if $t1 != -12345;
+
     printf OUT "%10s %8d %1d %1d %1d %1d %1d %5.1f %5.1f\n", $net_sta, $dist, $w1, $w2, $w3, $w4, $w5, $tp, $ts;
 }
+chdir "..";
