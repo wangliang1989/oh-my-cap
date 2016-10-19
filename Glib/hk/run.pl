11956b6f64dc54707b9837e0e03243b1aa3977f0
王亮 <wangliang0222@gmail.com>
2016-10-19 18:22:08 +0800

    fixes

--- Glib/hk/run.pl
+++ Glib/hk/run.pl
@@ -8,6 +8,9 @@ for (my $dist = 5; $dist <= 415; $dist = $dist + 5) {
 }
 
 for (my $depth = 5; $depth <= 30; $depth = $depth + 5) {
+    if ($depth < 10) {
+        $depth = "0$depth";
+    }
     # 计算双力偶
     system "fk.pl -Mhk/$depth/k -N512/0.2 -S2 @dist\n";
     # 计算爆炸源
