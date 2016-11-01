c42dcb22bf20d9fcfb2b6eae93e3ab2488222c69
王亮 <wangliang0222@gmail.com>
2016-11-01 08:23:30 +0800

    增加对权重文件的指定

--- example/inversion.pl
+++ example/inversion.pl
@@ -5,11 +5,10 @@ use warnings;
 @ARGV == 1 or die "Usage: perl $0 dirname";
 my ($event) = @ARGV;
 
-die "no weight.dat\n" if !-e "$event/weight.dat";
+my $weight = "weight.dat";# 指定权重文件
+die "no weight file\n" if !-e "$event/$weight";
 
 for (my $depth = 5; $depth <= 30; $depth = $depth + 5) {
-    if ($depth < 10) {
-        $depth = "0$depth";
-    }
-    system "cap.pl -H0.2 -P0.3 -S5/10/0 -T35/70 -D2/1/0.5 -C0.05/0.3/0.02/0.1 -W1 -X10 -Mmodel_$depth/5.0 $event";
+    $depth = "0$depth" if $depth < 10;
+    system "cap.pl -H0.2 -P0.3 -S5/10/0 -T35/70 -D2/1/0.5 -C0.05/0.3/0.02/0.1 -W1 -X10 -Mmodel_$depth/5.0 -Z$weight $event";
 }
