eb9185279f656e2951fc87c3722a33c2333f1e0f
王亮 <wangliang0222@gmail.com>
2016-11-02 19:42:45 +0800

    同时支持路径和文件

--- example/process.pl
+++ example/process.pl
@@ -4,7 +4,24 @@ use warnings;
 
 @ARGV >= 1 or die "Usage: perl $0 event1 event2 ... eventn\n";
 
-foreach my $event (@ARGV) {
+my @dir;
+foreach (@ARGV) {
+    if (-d $_) {
+        push @dir, $_;
+        next;
+    }
+    if (-e $_) {
+        open (IN,"< $_");
+        foreach (<IN>) {
+            my ($dir) = split /\s+/;
+            push @dir, $dir;
+        }
+        close(IN);
+        next;
+    }
+    print "can not find $_ !\n";
+}
+foreach my $event (@dir) {
     system "perl rdseed.pl $event";
     system "perl eventinfo.pl $event";
     system "perl marktime.pl $event";
