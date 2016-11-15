db4b6957d1040dbeccd0f81137f3664b25842cca
Dongdong Tian <seisman.info@gmail.com>
2016-11-15 21:06:54 +0800

    config parser v1

---
+++ example/config.pm
@@ -0,0 +1,59 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+sub read_config() {
+    my ($dir) = @_;
+
+    my %pars;
+    my $par_id = 0;
+    my $conf_file;
+
+    # 1st: project-wide configure file
+    $conf_file = "$ENV{OH_MY_CAP}/event.conf";
+    %pars = config_parser($conf_file, %pars) if -e $conf_file;
+
+    # 2nd: configure file in current directory
+    $conf_file = "./event.conf";
+    %pars = config_parser($conf_file, %pars) if -e $conf_file;
+
+    # 3rd: event-based configure file
+    $conf_file = "$dir/event.conf";
+    %pars = config_parser($conf_file, %pars) if -e $conf_file;
+
+    # check if all pars are valid
+    check_pars(%pars);
+
+    return %pars;
+}
+
+sub check_pars() {
+    my (%pars) = @_;
+
+    while (my ($key, $value) = each (%pars)) {
+        print "$key $value\n";
+    }
+    return 0;
+}
+
+sub config_parser() {
+    my ($config_file, %pars) = @_;
+    open(IN," < $config_file") or die "can not open configure file $config_file\n";
+    my @lines = <IN>;
+    close(IN);
+
+    foreach my $line (@lines) {
+        $line = substr $line, 0, (pos $line) - 1 if ($line =~ m/#/g);
+        chomp($line);
+        my ($key, $value) = split ":", $line;
+        next unless (defined($key) and defined($value));
+        $key = trim($key);
+        $value = trim($value);
+        $pars{$key} = $value;
+    }
+    return %pars;
+}
+
+sub  trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
+
+1;
--- example/inversion.pl
+++ example/inversion.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+require config;
 
 @ARGV >= 1 or die "Usage: perl $0 dirname";
 my @dir = @ARGV;
@@ -9,8 +10,26 @@ my $weight = "weight.dat";# 指定权重文件
 foreach my $event (@dir){
     die "no weight file\n" if !-e "$event/$weight";
 
-    for (my $depth = 5; $depth <= 30; $depth = $depth + 5) {
+    my %pars = read_config($event);
+    # 获取反演的震源深度
+    my @depth = split /\s+/, $pars{'DEPTH'};
+
+    foreach my $depth (@depth) {
         $depth = "0$depth" if $depth < 10;
-        system "cap.pl -H0.2 -P0.3 -S5/10/0 -T35/70 -D2/1/0.5 -C0.05/0.3/0.02/0.1 -W1 -X10 -Mmodel_$depth/5.0 -Z$weight $event";
+        my @command;
+        foreach my $key (sort keys %pars) {
+            # 不是反演需要的参数直接跳过
+            next unless ($key =~ /^-/);
+
+            if ($key eq '-M') {
+                my ($model, $mag) = split m/\//, $pars{$key};
+                push @command, "$key${model}_$depth/$mag";
+            } else {
+                push @command, "$key$pars{$key}";
+            }
+        }
+
+        print "cap.pl @command $event\n";
+        system "cap.pl @command $event";
     }
 }
