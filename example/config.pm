11adb9ec07018a41ec87b9983269cf0996defe64
王亮 <wangliang0222@gmail.com>
2016-11-26 16:33:46 +0800

    check the parameter

--- example/config.pm
+++ example/config.pm
@@ -20,9 +20,9 @@ sub read_config() {
     $conf_file = "$dir/event.conf";
     %pars = config_parser($conf_file, %pars) if -e $conf_file;
 
-    # fix argument of DEPTH
-    my @value = split /\s+/, $pars{"DEPTH"};
+    # parse arguments of DEPTH
     my @depth;
+    my @value = split /\s+/, $pars{"DEPTH"};
     foreach (@value) {
         if ($_ =~ m/\//g) {
             my ($start, $end, $delta) = split m/\//;
@@ -33,10 +33,14 @@ sub read_config() {
             push @depth, $_;
         }
     }
-    $pars{"DEPTH"} = join " ", @depth;
+    $pars{"DEPTH"} = join " ", sort { $a <=> $b } @depth;
 
     # setup arguments for cap
     $pars{'cap_args'} = setup_cap_args(%pars);
+
+    # check arguments
+    my $check_errors = check_args(%pars);
+    die "$check_errors errors found in event.conf\n" unless $check_errors == 0;
 
     return %pars;
 }
@@ -70,6 +74,48 @@ sub setup_cap_args() {
     }
 
     return $cap_args;
+}
+
+sub check_args() {
+    my (%pars) = @_;
+    my $error = 0;
+
+    my @freq = split /\s+/, $pars{"FREQ"};
+    my @filter = split m/\//, $pars{"-C"};
+
+    # DATA PREPROCESS
+    unless (($freq[3] * $pars{"DT"}) < 0.5) {
+        print STDERR "Error in freqlimits: not obey Nyqusit law\n";
+        $error++;
+    }
+
+    # Inversion
+    unless (($freq[1] <= $filter[0]) and ($filter[1] <= $freq[2])) {
+        print STDERR "filter range of Pnl does not fit the freqlimits\n";
+        $error++;
+    }
+    unless (($freq[1] <= $filter[2]) and ($filter[3] <= $freq[2])) {
+        print STDERR "filter range of Suface wave does not fit the freqlimits\n";
+        $error++;
+    }
+
+    my @depth = split /\s+/, $pars{"DEPTH"};
+    my $green = "$ENV{OH_MY_CAP}/Glib/$pars{'MODEL'}/$pars{'MODEL'}";
+    foreach my $grn (@depth) {
+        my $depth = $grn;
+        $depth = "0$grn" if ($depth < 10);
+        my (undef, $delta) = split /\s+/, `saclst delta f ${green}_$depth/$grn.grn.0`;
+        unless ($delta == $pars{"DT"}) {
+            print STDERR "DT is not equal with delta in green function in depth of $grn\n";
+            $error++;
+        }
+        unless ($delta == $pars{"-H"}) {
+            print "-H parameter is not equal with delta in green function in depth of $grn\n";
+            $error++;
+        }
+    }
+
+    return ($error);
 }
 
 sub trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
