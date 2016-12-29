3dbfe6093ce85340c61de8a4bad7c1550ebedf0f
王亮 <wangliang0222@gmail.com>
2016-12-29 16:08:49 +0800

    数据预处理时 cut 数据

--- example/marktime.pl
+++ example/marktime.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 use List::Util qw(min);
+use List::Util qw(max);
 $ENV{SAC_DISPLAY_COPYRIGHT} = 0;
 
 @ARGV == 1 or die "Usage: perl $0 dir\n";
@@ -15,15 +16,18 @@ chdir $dir;
 open(SAC, "| sac") or die "Error in opening SAC\n";
 print SAC "wild echo off\n";
 foreach my $Zfile (glob "*Z.SAC") {
-    my ($net, $sta) = split /\./, $Zfile;
-    my (undef, $evdp, $gcarc) = split /\s+/, `saclst evdp gcarc f $Zfile`;
-    my @time = split /\s+/, `taup_time -mod prem -ph P,p,Pn -h $evdp -deg $gcarc --time`;
+    my ($net, $sta) = split m/\./, $Zfile;
+    my (undef, $evdp, $gcarc, $b, $e) = split m/\s+/, `saclst evdp gcarc b e f $Zfile`;
+    my @time = split m/\s+/, `taup_time -mod prem -ph P,p,Pn -h $evdp -deg $gcarc --time`;
     my $t0 = min @time;
+    my $start = max (0, $b, ($t0 - 10));
+    my $end = min ($e, ($t0 + 600));
 
+    print SAC "cut $start $end\n";
     print SAC "r ${net}.${sta}.*.SAC\n";
     print SAC "ch t0 $t0\n";
     print SAC "ch kt0 Pnl\n";
-    print SAC "wh\n";
+    print SAC "write over\n";
 }
 print SAC "q\n";
 close(SAC);
