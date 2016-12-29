cd245aa3d1c3e5ecbc3ef080d961ffe6f8ca4582
王亮 <wangliang0222@gmail.com>
2016-12-29 12:13:05 +0800

    添加参数种类，修改部分参数规则

--- example/inversion.pl
+++ example/inversion.pl
@@ -12,7 +12,7 @@ foreach my $event (@dir){
     die "no weight file\n" if !-e "$event/$weight";
 
     # 获取反演的震源深度
-    my @depth = split /\s+/, $pars{'DEPTH'};
+    my @depth = split m/\s+/, $pars{'DEPTH'};
     foreach my $depth (@depth) {
         $depth = "0$depth" if $depth < 10;
         # deal with -M option
