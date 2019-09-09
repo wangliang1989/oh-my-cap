4a79c31d003dbddab7a1eb376ac5ba7ea604c846
wangliang <wangliang0222@gmail.com>
2019-09-09 13:43:30 +0800

    fix

--- Glib/checkgrn.pl
+++ Glib/checkgrn.pl
@@ -21,25 +21,17 @@ foreach my $fname (@config) {
         chdir "${model}_$dep" or die;
         print "${model}_$dep\n";
         foreach my $dist (@dist) {
-            #110.606.grn.5
-            #next unless (int($dist/5) == ($dist/5));
-            open(SAC, "|sac ") or die "Error in opening sac\n";
-            print SAC "wild echo off\n";
-            print SAC "r $dist.grn.?\n";
-            print SAC "write over\n";
-            print SAC "q\n";
-            close(SAC);
-            my ($start, $end) = (split m/\s+/, `saclst b e f ${dist}.grn.0`)[1..2];;
-            system "gmt begin";
-            system "gmt figure junk pdf A1c";
-            &gmt_draw($dist, $dep, $start, $end, 7, " 1c", " 0c");
-            &gmt_draw($dist, $dep, $start, $end, 8, "15c", " 0c");
-            &gmt_draw($dist, $dep, $start, $end, 5, " 1c", "10c");
-            &gmt_draw($dist, $dep, $start, $end, 6, "15c", "10c");
-            &gmt_draw($dist, $dep, $start, $end, 3, " 1c", "20c");
-            &gmt_draw($dist, $dep, $start, $end, 4, "15c", "20c");
-            &gmt_draw($dist, $dep, $start, $end, 0, " 1c", "30c");
-            &gmt_draw($dist, $dep, $start, $end, 1, "15c", "30c");
+            &refresh_sac_headers($dist);
+            system "gmt begin junk pdf A1c";
+            system "gmt subplot begin 3x4 -Fs8c/4c -M0.5c/1c -T'Green Functions of $dist km'";
+            foreach my $q (0, 1, 2, 3, 4, 5, 6, 7, 8, 'a', 'b', 'c') {
+                my $num = $q;
+                $num = 9 if ($q eq 'a');
+                $num = 10 if ($q eq 'b');
+                $num = 11 if ($q eq 'c');
+                &gmt_draw($dist, $q, $num);
+            }
+            system "gmt subplot end";
             system "gmt end";
             system "mv junk.pdf ${dist}.pdf";
             push @cpdf, "${dist}.pdf";
@@ -51,26 +43,40 @@ foreach my $fname (@config) {
     chdir ".." or die;
 }
 
+sub refresh_sac_headers () {
+    my ($dist) = @_;
+    open(SAC, "|sac ") or die "Error in opening sac\n";
+    print SAC "wild echo off\n";
+    print SAC "r $dist.grn.?\n";
+    print SAC "wh\n";
+    print SAC "q\n";
+    close(SAC);
+}
 sub gmt_draw () {
-    my ($dist, $dep, $start, $end, $q, $locx, $locy) = @_;
-    $locx = &trim($locx);
-    $locy = &trim($locy);
+    my ($dist, $q, $num) = @_;
+    my ($b, $e, $min, $max, $t1, $t2) = &getrange($dist, $q);
+    system "gmt subplot set $num";
+    system "gmt basemap -R$b/$e/$min/$max -Baf -BWSen+t'${dist}.grn.${q}'";
+    system "gmt sac  -W1p,black ${dist}.grn.${q}";
+    &gmtline ($t1, "1p,blue", $min, $max);
+    &gmtline ($t2, "1p,red", $min, $max);
+}
+sub getrange () {
+    my ($dist, $q) = @_;
+    my ($b, $e, $t1, $t2) = (split m/\s+/, `saclst b e t1 t2 f ${dist}.grn.$q`)[1..4];
     my ($min) = (split m/\s+/, `saclst depmin f ${dist}.grn.$q`)[1];
     my ($max) = (split m/\s+/, `saclst depmax f ${dist}.grn.$q`)[1];
     $min = $min * 1.1;
     $max = $max * 1.1;
-    my ($b, $e, $t1, $t2) = (split m/\s+/, `saclst b e t1 t2 f ${dist}.grn.0`)[1..4];
-    $b = max($b, ($start - 2 * ($end - $start)));
-    $e = min($e, ($end + 2 * ($end - $start)));
-    system "gmt sac ${dist}.grn.${q} -JX12c/6c -R$b/$e/$min/$max -Bxaf -Byaf -BWSen+t'${dist}.grn.${q}' -W1p,black -Xa$locx -Ya$locy";
-    &gmtline ($start, "1p,darkblue,-", "-Xa$locx -Ya$locy -N");
-    &gmtline ($end, "1p,darkblue,-", "-Xa$locx -Ya$locy -N");
-    &gmtline ($t1, "1p,blue", "-Xa$locx -Ya$locy -N");
-    &gmtline ($t2, "1p,red", "-Xa$locx -Ya$locy -N");
+    if (($min == 0) and ($max == 0)) {
+        $min = -1;
+        $max = 1;
+    }
+    return ($b, $e, $min, $max, $t1, $t2);
 }
 sub gmtline (){
-    my ($t, $w, $cmd) = @_;
-    open (GMT, "| gmt plot -W$w $cmd") or die;
-    print GMT "$t 12345\n $t -12345\n";
+    my ($t, $w, $min, $max) = @_;
+    open (GMT, "| gmt plot -W$w") or die;
+    print GMT "$t $min\n $t $max\n";
     close (GMT);
 }
